05ea70fce5f34960e9c2f5c437dfb95cfcdac8ce
MartyVos <marty.vos@student.hu.nl>
2018-11-22 14:18:47 +0100

    Added Doxygen Documentation

--- lib/EncodeDecodeMSG.hpp
+++ lib/EncodeDecodeMSG.hpp
@@ -6,9 +6,22 @@
 
 class EncodeDecodeMSG{
 public:
+	///@fn EncodeDecodeMSG::EncodeDecodeMSG()
+    ///@brief The constructor for the EncodeDecodeMSG class.
+    ///@details This constructor creates a EncodeDecodeMSG object.
 	EncodeDecodeMSG(){};
 	
-	uint16_t EncodeMessage(Message msg);
+	///@fn uint16_t EncodeDecodeMSG::EncodeMessage(Message msg)
+    ///@brief Encodes message.
+    ///@details This method encodes a @c Message object and returns the encoded message.
+	///@param msg The message to encode.
+	///@return emsg The encoded message.
+	
+	///@fn EncodeDecodeMSG::DecodeMessage(uint16_t encodedMessage)
+    ///@brief Decodes a message.
+    ///@details This method decodes a unsigned 16-bit integer message and returns a @c Message object.
+	///@param encodedMessage The message to decode.
+	///@return msg The decoded message.
 	Message DecodeMessage(uint16_t encodedMessage);
 };
 
